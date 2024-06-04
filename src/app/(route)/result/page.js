6a89fdd22cddfcd6d25970586f866a088a0ee7ee
Koo In Young <inzerokoo@gmail.com>
2024-06-04 13:11:54 +0900

    fix: Lint

--- src/app/(route)/result/page.js
+++ src/app/(route)/result/page.js
@@ -25,12 +25,12 @@ export default function Result() {
       <ThemeProvider theme={theme}>
         <Nav select={'create'} />
         <ResultWrap>
-            <CheckCircleIcon/>
-            <h2>
-                데이터 전송이 모두 완료되었습니다. <br/>
-                작업물은 24시간내에 이메일로 전달드립니다.
-            </h2>
-            <SolidBtn text={"메인으로 돌아가기"} link={"/"}/>
+          <CheckCircleIcon />
+          <h2>
+            데이터 전송이 모두 완료되었습니다. <br />
+            작업물은 24시간내에 이메일로 전달드립니다.
+          </h2>
+          <SolidBtn text={'메인으로 돌아가기'} link={'/'} />
         </ResultWrap>
 
         <Footer />
@@ -40,26 +40,25 @@ export default function Result() {
 }
 
 const ResultWrap = styled(Box)`
-    padding-top: 10rem;
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    svg{
-        color: #ec34a3;
-        width: 20rem;
-        height: 20rem;
-    }
-    h2{
-        color: #fff;
-        font-size: 2rem;
-        font-weight: bold;
-        text-align: center;
-        line-height: 150%;
-        margin: 2rem 0 5rem 0;
-    }
+  padding-top: 10rem;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  svg {
+    color: #ec34a3;
+    width: 20rem;
+    height: 20rem;
+  }
+  h2 {
+    color: #fff;
+    font-size: 2rem;
+    font-weight: bold;
+    text-align: center;
+    line-height: 150%;
+    margin: 2rem 0 5rem 0;
+  }
   @media ${() => theme.device.mobile} {
-
   }
 `;
